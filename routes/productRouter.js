a50b0834010f1f204ba0f030e505d53347de9aa4
Nada120 <nada50tarek@gmail.com>
2023-07-26 10:15:46 +0300

    create deleteProduct route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const adminAuth = require('../middleware/authAdmin');
 const addProduct = require('../services/adminServices/addProduct');
 const getAllProducts = require('../services/adminServices/getAllProducts');
-const err = require('../middleware/errorHadle');
+const deleteProduct = require('../services/adminServices/deleteProduct');
 
 // admin services
 router.post('/', adminAuth, addProduct);
 router.get('/', adminAuth, getAllProducts);
-// router.delete('/delete/:id', adminAuth, deleteProduct);
+router.delete('/delete/:id', adminAuth, deleteProduct);
 // router.patch('/edit/:id', adminAuth, editProduct);
 
 module.exports = router;
---
+++ services/adminServices/deleteProduct.js
@@ -0,0 +1,25 @@
+const productModel = require('../../model/productModel');
+const err = require('../../middleware/errorHadle');
+
+const deleteProduct = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const delProduct = await productModel.findOneAndDelete({ _id: id });
+
+        if (!delProduct) {
+            next(err({
+                message: 'There Is No Product has This Id',
+                stateCode: 400
+            }));
+        } else {
+            res.send('Delete Successfully');
+        }
+    } catch (e) {
+        next(err({
+            message: e.message,
+            stateCode: 404
+        }));
+    }
+}
+
+module.exports = deleteProduct;
