763cd5dbb613b79aa205273f809a53e26425b779
Nada120 <nada50tarek@gmail.com>
2023-07-29 10:13:37 +0300

    edit deleteProduct route

--- services/adminServices/deleteProduct.js
+++ services/adminServices/deleteProduct.js
@@ -6,6 +6,7 @@ const err = require('../../middleware/errorHadle');
 const deleteProduct = async (req, res, next) => {
     try {
         const {id} = req.params;
+
         const product = await productModel.findOne({ _id: id });
         if (!product) {
             next(err({
@@ -16,7 +17,8 @@ const deleteProduct = async (req, res, next) => {
             const users = await userModel.find({});
             for (const el of users) {
                 let cart = el.myCart.filter(l => l != id);
-                let verify = user.Verify.filter(l => l.idPro != id);
+                let verify = el.Verify.filter(l => l.idPro != id);
+                
                 await userModel.findByIdAndUpdate(
                     {_id: el._id},
                     {myCart: cart,
